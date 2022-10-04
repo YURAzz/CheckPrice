d6f55654fe468ead9bd267cb4831d36cd521e987
YURAzz <yuri102011@hotmail.it>
2022-10-04 16:55:05 +0200

    commit 2

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
-
+import java.util.concurrent.CountDownLatch;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -19,6 +19,7 @@ import org.apache.xpath.CachedXPathAPI;
 import org.apache.xpath.operations.Or;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class App {
@@ -38,11 +39,24 @@ public class App {
     Integer i = 0;
 
     String schedulationPath = "/content/group/definition/list[@name='schedulations']/value/schedulation";
-    String fruitionPath = "//group/definition/taxonomylink/value[@descr='EST'] | //group/definition/taxonomylink/value[@descr='DTO']";
+    String EstDto = "./group/definition/taxonomylink/value";
+    String fruitionPath = "./list[@name='fruitionList']/value/fruition";
+    String labelPath = "//taxonomylink[@name='toResolutionTax']/value/ti/@label";
+    String idPath = "./taxonomylink[@name='toPriceTax']/value/ti/@id ";
 
     try {
 
         NodeList schedulationList = v.selectNodeList(doc, schedulationPath);
+        NodeList estDto = v.selectNodeList(schedulationList.item(i), EstDto);
+
+        try {
+          Result output = new StreamResult(new File("output.xml"));
+          Transformer transformer = TransformerFactory.newInstance().newTransformer();
+          Source input = new DOMSource(schedulationList.item(0));
+          transformer.transform(input, output);
+      } catch ( TransformerFactoryConfigurationError | TransformerException e) {
+          e.printStackTrace();
+      }
 
         for (i = 0; i < schedulationList.getLength(); i++) {
 
@@ -50,9 +64,14 @@ public class App {
 
           for (int k = 0; k < fruitionNodes.getLength(); k++){
 
-                  Element currentTypeEl = (Element) fruitionNodes.item(k);
+                  Element currentTypeEl = (Element) estDto.item(k);
                   String currentType = currentTypeEl.getAttribute("descr");
-                  System.out.println(currentType);
+                  NodeList path = v.selectNodeList(fruitionNodes.item(k), labelPath);
+                  String currentKey = path.item(k).toString();
+                  NodeList ids = v.selectNodeList(schedulationList.item(i), idPath);
+                  String id = ids.item(k).toString();
+
+
 
             }
           }
