7487765fe57af15cd1b365c835e9f6334d4f41a5
Yuri Loayza <lllyuro20@gmail.com>
2022-10-05 11:33:21 +0100

    final

--- src/App.java
+++ src/App.java
@@ -1,26 +1,13 @@
 import java.io.File;
+import java.util.Set;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Set;
-import java.util.concurrent.CountDownLatch;
-
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.apache.xpath.CachedXPathAPI;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.Result;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.TransformerFactoryConfigurationError;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
-import org.apache.xpath.CachedXPathAPI;
-import org.apache.xpath.operations.Or;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 public class App {
 
@@ -39,46 +26,50 @@ public class App {
     Integer i = 0;
 
     String schedulationPath = "/content/group/definition/list[@name='schedulations']/value/schedulation";
-    String EstDto = "./group/definition/taxonomylink/value";
+    String EstDto = "./group/definition/taxonomylink/value/@descr";
     String fruitionPath = "./list[@name='fruitionList']/value/fruition";
-    String labelPath = "//taxonomylink[@name='toResolutionTax']/value/ti/@label";
-    String idPath = "./taxonomylink[@name='toPriceTax']/value/ti/@id ";
+    String labelPath = "./list/value/fruition/taxonomylink[@name='toResolutionTax']/value/ti/@label";
+    String idPath = "./list/value/fruition/taxonomylink[@name='toPriceTax']/value/ti/@id ";
 
     try {
 
         NodeList schedulationList = v.selectNodeList(doc, schedulationPath);
-        NodeList estDto = v.selectNodeList(schedulationList.item(i), EstDto);
-
-        try {
-          Result output = new StreamResult(new File("output.xml"));
-          Transformer transformer = TransformerFactory.newInstance().newTransformer();
-          Source input = new DOMSource(schedulationList.item(0));
-          transformer.transform(input, output);
-      } catch ( TransformerFactoryConfigurationError | TransformerException e) {
-          e.printStackTrace();
-      }
-
+        
         for (i = 0; i < schedulationList.getLength(); i++) {
-
+          
           NodeList fruitionNodes = v.selectNodeList(schedulationList.item(i), fruitionPath);
+          NodeList estDto = v.selectNodeList(schedulationList.item(i), EstDto);
+          String currentType = estDto.item(0).getTextContent();
+
+          if (currentType.equals("DTO") || currentType.equals("EST")) {
 
           for (int k = 0; k < fruitionNodes.getLength(); k++){
 
-                  Element currentTypeEl = (Element) estDto.item(k);
-                  String currentType = currentTypeEl.getAttribute("descr");
-                  NodeList path = v.selectNodeList(fruitionNodes.item(k), labelPath);
-                  String currentKey = path.item(k).toString();
+                  NodeList path = v.selectNodeList(schedulationList.item(i), labelPath);
                   NodeList ids = v.selectNodeList(schedulationList.item(i), idPath);
-                  String id = ids.item(k).toString();
+                  String currentKey = path.item(k).getTextContent();
+                  String currentId = ids.item(k).getTextContent();
 
+                  if (resolutionPrices.containsKey(currentKey))
+                  {
+                    String check = resolutionPrices.get(currentKey).toString();
 
-
+                    if (check.equals(currentId))
+                    {}
+                    else
+                    {
+                      errorSet.add("Per la quality "+ currentKey + " di fruition " + currentType + 
+                      " sono presenti price category differenti, currentKey mismatch: " + check + " " + currentId );
+                    }
+                  }
+                  else
+                  resolutionPrices.put(currentKey, currentId);
             }
           }
-    } catch (TransformerException e1) {
-        e1.printStackTrace();
+        }
+    } catch (TransformerException e) {
+        e.printStackTrace();
       }
-
     return doc;
   }
 }
